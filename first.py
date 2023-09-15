291bf74109c26d8cae906df7affe49f59b45f287
Shubha G (EXT-Nokia) <shubha.g.ext@nokia.com>
2023-09-15 11:37:16 +0530

    second commit

--- first.py
+++ first.py
@@ -1,1 +1,17 @@
-print("hello world")
+print("hello world  ")
+
+
+
+"""
+git init ---- initialises an empty repository
+gir status ---- checks the status of untracked files 
+git add . ----- adds all files in the current directory
+git commit -m "commit message" ----- commits all the changes
+git log ---- logs all the hashes of the commits
+git checkout h12343124jn3j431k4j ----- checksout to the commit 
+git branch -a ----- lists all branches
+git checkout branchname --- moves to a branch (check)
+git push origin main --- pushes online to github
+git clone www.sdfds.git --- clones the entire git repo
+
+"""
